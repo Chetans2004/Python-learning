0a93953597cde39ebb2973b440ea2183dd1d1bed
Chetans2004 <chetanshinagari@gmail.com>
2025-09-22 23:50:35 +0530

    update Identifiers

--- Day1.py
+++ Day1.py
@@ -1,3 +1,13 @@
 #Keywords are reserved words(Predefined words) such as def,for,if etc...
 import keyword
 print(keyword.kwlist) #it will give all keywords list
+
+
+#Identifiers are name given to entity like functiion and variables
+'''Rules of Identifiers
+    1.Names can't be start with Digit
+    2.we can't be use special character Except underscore
+    3.Keywords can't be use as identifiers 
+    '''
+    
+
