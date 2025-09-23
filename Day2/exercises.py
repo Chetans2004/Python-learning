fab3e745b7ba085f80296a71acd1d20df40b6330
Chetans2004 <chetanshinagari@gmail.com>
2025-09-23 20:09:44 +0530

    Add README with Day 2: Variables and Data Types

---
+++ Day2/exercises.py
@@ -0,0 +1,8 @@
+#Simple Greeting
+print("Hello" + input("Enter Your name") + "Welcome to Python")
+
+print(input("Enter Your name"))
+n=input("Enter number")
+n1=input("Ente another number")
+ans=n+n1
+print(ans)
---
+++ Day2/input.py
@@ -0,0 +1,5 @@
+#The input() function in Python is used to take input from the user. It waits for the user 
+# to type something on keyboard and once user presses Enter, it returns the value.
+#By default, input() always returns data as a string, even if you enter numbers
+
+print(input("What is your name"))
