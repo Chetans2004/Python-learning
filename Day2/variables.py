e52c580afd9488f476bc8121d01795ef07317337
Chetans2004 <chetanshinagari@gmail.com>
2025-09-24 21:54:48 +0530

    Updated

--- Day2/variables.py
+++ Day2/variables.py
@@ -3,7 +3,7 @@
 # name=input("Enter your name")
 # print(name)
 
-#Simple Interest Calculato
+#Simple Interest Calculator
 # amount=int(input("Enetr Principle  amount"))
 # rate=int(input("Enter Rate of intrest"))
 # time=int(input("Time in Years"))
@@ -11,7 +11,8 @@
 # print(SI) 
 
 #Student Info
-name=(input("Enter you name"))
-age=(input("Enter your age"))
-college=(input("Enter your collge name"))
+# name=(input("Enter you name"))
+# age=(input("Enter your age"))
+# college=(input("Enter your collge name"))
 print("My name is" + name + "I am" + age +"year old." +"I study at" + college  ) 
+
