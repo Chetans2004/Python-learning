f1886bb66973f1524625d60ebe782c2264893ac1
Chetans2004 <chetanshinagari@gmail.com>
2025-09-26 19:18:54 +0530

    Learned about String

---
+++ Day4/string.py
@@ -0,0 +1,24 @@
+# #Create a string variable with your name and print its length.
+# name="Chetan"
+# print(len(name))
+
+#2.Take a user input string and print the first and last characters.
+# string=input("Enter your name")
+
+# print("First Character of your name is:",string[0] , "Last Character of your name is",string[-1])
+
+#3.Given s = "HelloWorld", print:
+#The first character
+#The last character
+#The middle character
+# s="HelloWorld"
+# print("First character:", s[0])
+# print("Last character:", s[-1])
+# length=len(s)
+# middle_index=length//2
+# print(s[middle_index])
+
+#4.Write a program that asks for a string and prints only the characters at even positions
+# string = input("Enter string")
+# print("characters at even positions is ", string[::2])
+
