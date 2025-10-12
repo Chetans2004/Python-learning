5951d253413e2780da271db21a7b98f6129716db
Chetans2004 <chetanshinagari@gmail.com>
2025-10-12 23:36:36 +0530

    In day13 learned about functions adn how its work..

---
+++ Day13/function_with_parameter.py
@@ -0,0 +1,9 @@
+#A parameter variable inside a function that receive the data or input from outside
+#When we call function with parameter we need to provide the aguments
+ 
+## Difference between parameter and arguments
+#Whenever we difine a variable inside a function that is parameter
+#Actual valus we pass the functions its calling time it is arguments
+def greet(name):
+    print(f"Hi {name}")
+greet("Chetan")
---
+++ Day13/functions.py
@@ -0,0 +1,10 @@
+#A function is a block of code that perform specific task when it is called.
+#And we can br reused in the other part of iur code whenevre we want
+#The function is only works when you call the function
+#Its helps to avoid the repeating the the task and make a program to easier to understand
+
+
+#Examples
+def greet():
+    print("Helllo")
+greet()
